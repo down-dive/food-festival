edc7f67be721bbb1fdf4e7763ab541b2f82019ed
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-05-30 17:37:26 -0700

    settung up webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
+const { dependencies } = require('webpack');
 
 module.exports = {
     entry: './assets/js/script.js',
     output: path.resolve(__dirname, 'dist'),
-    filename: 'main.bundle.js'
+    filename: 'main.bundle.js',
+    mode: 'development'
 };
